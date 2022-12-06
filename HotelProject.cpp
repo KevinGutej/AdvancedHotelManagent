7deaf769653e32fd67f30b3360cdf11a98fa3acb
KevinGutej <76398856+KevinGutej@users.noreply.github.com>
2022-12-06 23:03:06 +0100

    Choosing room function complete with more

--- HotelProject.cpp
+++ HotelProject.cpp
@@ -77,58 +77,140 @@ Client getClientDetail()
 void printAvaiableRooms(int roomSize, RoomList listOfRooms)
 {
 	int i;
+	if(roomSize == 1)
+	{
+		for(i=0; i < 10; i++)
+		{
+			cout << "Room Number: " << listOfRooms.singleRooms[i].roomNumber << endl;
+			cout << "Price: " << listOfRooms.singleRooms[i].price << endl;
+			cout << "Room Size: " << listOfRooms.singleRooms[i].roomSize << endl;
+			if(listOfRooms.singleRooms[i].clients[0].firstName != "")
+			{
+				cout << "Room is Occupied" << endl;
+			}
+			else {
+				cout << "Room is Available" << endl;
+			}
+			cout << "---------------------- " << endl;
+		}	
+	}
+		
+	
+	else if(roomSize == 2)
+	{
+		for(i=0; i < 6; i++)
+		{
+			cout << "Room Number: " << listOfRooms.doubleRooms[i].roomNumber << endl;
+			cout << "Price: " << listOfRooms.doubleRooms[i].price << endl;
+			cout << "Room Size: " << listOfRooms.doubleRooms[i].roomSize << endl;
+			if(listOfRooms.doubleRooms[i].clients[0].firstName != "")
+			{
+				cout << "Room is Occupied" << endl;
+			}
+			else {
+				cout << "Room is Available" << endl;
+			}
+			cout << "---------------------- " << endl;
+		}
+	}
+	
+	else if(roomSize == 3)
+	{
+		for(i=0; i < 3; i++)
+		{
+			cout << "Room Number: " << listOfRooms.tripleRooms[i].roomNumber << endl;
+			cout << "Price: " << listOfRooms.tripleRooms[i].price << endl;
+			cout << "Room Size: " << listOfRooms.tripleRooms[i].roomSize << endl;
+			if(listOfRooms.tripleRooms[i].clients[0].firstName != "")
+			{
+				cout << "Room is Occupied " << endl;
+			}
+			else {
+				cout << "Room is Available" << endl;
+			}
+			cout << "---------------------- " << endl;
+		}
+	}
+	else
+	{
+		cout << "Room size is not available" << endl;
+	}
+	
+}
+
+bool checkIfRoomNumberIsAvailable(int roomNumber, RoomList listOfRooms)
+{
+	int i;
 	for(i=0; i < 10; i++)
 	{
-		cout << "Room Number: " << listOfRooms.singleRooms[i].roomNumber << endl;
-		cout << "Price: " << listOfRooms.singleRooms[i].price << endl;
-		cout << "Room Size: " << listOfRooms.singleRooms[i].roomSize << endl;
-		if(listOfRooms.singleRooms[i].clients[0].firstName != "")
+		if(listOfRooms.singleRooms[i].roomNumber == roomNumber)
 		{
-			cout << "Room is Occupied" << endl;
-		}
-		else {
-			cout << "Room is Available" << endl;
-		}
-		cout << "---------------------- " << endl;
+			if(listOfRooms.singleRooms[i].clients[0].firstName == "")
+			{
+				return true;	
+			}
+			else
+			{
+				cout << "This room is not available, enter again" << endl;
+				return false;
+			}
+		}		
 	}
 	
 	for(i=0; i < 6; i++)
 	{
-		cout << "Room Number: " << listOfRooms.doubleRooms[i].roomNumber << endl;
-		cout << "Price: " << listOfRooms.doubleRooms[i].price << endl;
-		cout << "Room Size: " << listOfRooms.doubleRooms[i].roomSize << endl;
-		if(listOfRooms.doubleRooms[i].clients[0].firstName != "")
+		if(listOfRooms.doubleRooms[i].roomNumber == roomNumber)
 		{
-			cout << "Room is Occupied" << endl;
+			if(listOfRooms.doubleRooms[i].clients[0].firstName == "")
+			{
+				return true;	
+			}
+			else
+			{
+				cout << "This room is not available, enter again" << endl;
+				return false;
+			}
 		}
-		else {
-			cout << "Room is Available" << endl;
-		}
-		cout << "---------------------- " << endl;
 	}
 	
-	for(i=0; i < 3; i++)
+	for(i=0; i < 3;  i++)
 	{
-		cout << "Room Number: " << listOfRooms.tripleRooms[i].roomNumber << endl;
-		cout << "Price: " << listOfRooms.tripleRooms[i].price << endl;
-		cout << "Room Size: " << listOfRooms.tripleRooms[i].roomSize << endl;
-		if(listOfRooms.tripleRooms[i].clients[0].firstName != "")
+		if(listOfRooms.tripleRooms[i].roomNumber == roomNumber)
 		{
-			cout << "Room is Occupied " << endl;
+			if(listOfRooms.tripleRooms[i].clients[0].firstName == "")
+			{
+				return true;	
+			}
+			else
+			{
+				cout << "This room is not available, enter again" << endl;
+				return false;
+			}
 		}
-		else {
-			cout << "Room is Available" << endl;
-		}
-		cout << "---------------------- " << endl;
 	}
+	cout << "This room does not exist, enter again" << endl;
+	return false;
 }
+
 
 void roomBookingMenu(RoomList listOfRooms)
 {
 	int roomSize;
-	cout << "What room size would you like(1,2,3): " << endl;
-	cin >> roomSize;
-	printAvaiableRooms(roomSize,listOfRooms);
+	int roomNumber;
+	do
+	{
+		cout << "What room size would you like(1,2,3): " << endl;
+		cin >> roomSize;
+		printAvaiableRooms(roomSize,listOfRooms);
+	}while(roomSize > 3 || roomSize < 0);
+	
+	do
+	{
+		cout << "Which room number would you like?: ";
+		cin >> roomNumber;
+	}
+	while(checkIfRoomNumberIsAvailable(roomNumber, listOfRooms) == false);
+	
 }
 
 int main()
@@ -148,8 +230,8 @@ int main()
 	
 	
 	
-	Client firstClient = getClientDetail();
-	display(firstClient);
+	//Client firstClient = getClientDetail();
+	//display(firstClient);
 	
 	roomBookingMenu(listOfRooms);
 	
